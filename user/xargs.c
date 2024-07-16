46b6150c3cb7844855b1c516fc07eed430a75fdb
PikaChu231121 <18250670351@163.com>
2024-07-15 19:32:13 -0700

    Tiny change

--- user/xargs.c
+++ user/xargs.c
@@ -27,7 +27,7 @@ int main(int argc, char *argv[])
 {
     if (argc <= 1)
     {
-        printf("usage: xargs command [initial-args]\n");
+        printf("Usage: xargs command [initial-args]\n");
         exit(1);
     }
     char buf[512];
